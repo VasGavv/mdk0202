30f953325b4b4c7d8e6099b3ae4cad901bf7767a
Yaroslav <yaroslavvasilev52615@gmail.com>
2026-09-04 20:28:23 +0900

    Доюбавил комментарий

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ a = int(input('Введите число: '))
 b = int(input('Введите второе число: '))
 
 print(f"Сумма {a} + {b} = {a + b}")
+
+from datetime import datetime
+print("Сегодня дата:", datetime.now().date())
+
+#commentaryi
